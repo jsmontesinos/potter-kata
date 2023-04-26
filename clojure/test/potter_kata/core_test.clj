d1f4d477b67982b65bb75b299c51f9b34e20bd53
Juan Sánchez <jsmontesinos@gmail.com>
2023-04-26 19:56:47 +0200

    Same should happen for other books

--- clojure/test/potter_kata/core_test.clj
+++ clojure/test/potter_kata/core_test.clj
@@ -7,5 +7,21 @@
     (is (= (calculateBasketPrice []) 0))))
 
 (deftest one
-  (testing "Should return 8 when called with a basket with 1 book"
+  (testing "Should return 8 when called with a basket book 1"
     (is (= (calculateBasketPrice [1]) 8))))
+
+(deftest two
+  (testing "Should return 8 when called with a basket book 2"
+    (is (= (calculateBasketPrice [2]) 8))))
+
+(deftest three
+  (testing "Should return 8 when called with a basket book 3"
+    (is (= (calculateBasketPrice [3]) 8))))
+
+(deftest four
+  (testing "Should return 8 when called with a basket book 4"
+    (is (= (calculateBasketPrice [4]) 8))))
+
+(deftest five
+  (testing "Should return 8 when called with a basket book 5"
+    (is (= (calculateBasketPrice [5]) 8))))
