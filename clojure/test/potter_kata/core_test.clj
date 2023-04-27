0b02d546d37af28410fabb913ee85aa3a6f53b49
Juan Sánchez <jsmontesinos@gmail.com>
2023-04-27 14:44:48 +0200

    Switch to parametrized test

--- clojure/test/potter_kata/core_test.clj
+++ clojure/test/potter_kata/core_test.clj
@@ -2,26 +2,19 @@
   (:require [clojure.test :refer :all]
             [potter-kata.core :refer :all]))
 
-(deftest empty
+(deftest emptyBasket
   (testing "should return 0 when called with an empty basket"
     (is (= (calculateBasketPrice []) 0))))
 
-(deftest one
-  (testing "Should return 8 when called with a basket book 1"
-    (is (= (calculateBasketPrice [1]) 8))))
+(deftest oneBook
+  (testing "Should return 8 when called with a basket with a book of some type"
+    (are [bookType] (= (calculateBasketPrice [bookType]) 8)
+      1
+      2
+      3
+      4
+      5)))
 
-(deftest two
-  (testing "Should return 8 when called with a basket book 2"
-    (is (= (calculateBasketPrice [2]) 8))))
-
-(deftest three
-  (testing "Should return 8 when called with a basket book 3"
-    (is (= (calculateBasketPrice [3]) 8))))
-
-(deftest four
-  (testing "Should return 8 when called with a basket book 4"
-    (is (= (calculateBasketPrice [4]) 8))))
-
-(deftest five
-  (testing "Should return 8 when called with a basket book 5"
-    (is (= (calculateBasketPrice [5]) 8))))
+(deftest threeCopiesBook1
+  (testing "Should return 8 * 3 when called with a basket of three copies of book 1"
+    (is (= (calculateBasketPrice [1 1 1]) (* 8 3)))))
